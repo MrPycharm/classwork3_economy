30a812182d6a7942eac04ac34f8373aba588ab9d
AShi <a.shilenko7@gmail.com>
2026-02-07 14:04:14 -0500

    customizing_2

--- mean_deviation.cpp
+++ mean_deviation.cpp
@@ -2,19 +2,11 @@
 
 std::vector<double> inputSample(int rows, int columns)
 {
-
     std::vector<double> storage(rows * columns);
-
     for (int idx= 0; idx < rows * columns; ++idx)
     {
         std::cin >> storage[idx];
     }
-
-    // for (int jdx = 0; jdx < rows * columns; ++jdx)
-    // {
-    //     std::cout << storage[jdx] << " ";
-
-    // }
     return storage;
 }
 
@@ -25,14 +17,12 @@ int mean(std::vector<double> vect, int length)
     {
         result += vect[idx];
     }
-    
     return result / length;
 }
 
 double dispertion(std::vector<double> vect, double mean, int length) 
 {
     double result = 0;
-
     for (int idx = 0; idx < length; ++idx)
     {
         result += ((vect[idx] - mean) * (vect[idx] - mean));
@@ -47,20 +37,15 @@ double mean_deviation(std::vector<double> storage, int rws, int col)
 {
     double mean_value = mean(storage, rws * col);
     double mean_deviation_value = dispertion(storage, mean_value, rws * col);
-
-    // std::cout << mean_deviation_value << "\n";
     return mean_deviation_value;
 }
 
-int main() 
+int main() // mean_deviation_with_input 
 {
     int rows, columns;
     std::cout << "Input rows and columns" << "\n";
     std::cin >> rows >> columns;
     std::vector<double> storage = inputSample(rows, columns);
-
     double result = mean_deviation(storage, rows, columns);
     std::cout << result << "\n";
-
 }
- 
